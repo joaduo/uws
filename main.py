66bd2640c1ba87ace96257130b51703309109ab2
joaduo <joaduo@gmail.com>
2024-03-11 22:24:57 -0300

    plugin example

--- main.py
+++ main.py
@@ -1,49 +1,14 @@
 import log
 import asyncio
-from uws import Server, POST, serve_file
+from uws import Server, serve_file
+from plugins.log_plugin import log_plugin
 
 
 AUTH_TOKEN='MYTOKEN'
-
 app = Server(static_path='/static/',
              auth_token=AUTH_TOKEN,
              port=8000,
              )
-
-
-@app.json()
-async def token(method, new_token):
-    if method == POST:
-        app.auth_token = new_token
-    return ''
-
-
-@app.json()
-async def log_level(method, level):
-    if method == POST:
-        log.LOG_LEVEL = log.LABEL_TO_INT.get(level, log.INFO)
-    return log.INT_TO_LABEL.get(log.LOG_LEVEL, 'undefined for {}'.format(log.LOG_LEVEL))
-
-
-@app.plain()
-async def logs(verb, _):
-    return log.stream_web_log()
-
-
-@app.plain()
-async def log_freq(verb, _):
-    return log.stream_web_log_frequency()
-
-
-@app.json()
-async def logcfg(verb, cfg):
-    if verb == POST:
-        log.LOG_LEVEL = cfg.get('log_level', log.LOG_LEVEL)
-        log.WEB_LOG_LEVEL = cfg.get('web_log_level', log.WEB_LOG_LEVEL)
-        log.WEB_LOG_SIZE = cfg.get('web_log_size', log.WEB_LOG_SIZE)
-    return dict(log_level=log.LOG_LEVEL,
-                web_log_level=log.WEB_LOG_LEVEL,
-                web_log_size=log.WEB_LOG_SIZE)
 
 
 @app.html('/')
@@ -56,6 +21,7 @@ def main():
     log.LOG_LEVEL = log.INFO
     log.garbage_collect()
     try:
+        app.mount(log_plugin)
         asyncio.run(app.run())
         loop = asyncio.get_event_loop()
         loop.run_forever()
---
+++ plugins/log_plugin.py
@@ -0,0 +1,39 @@
+from uws import Plugin, POST
+import log
+
+
+log_plugin = Plugin()
+
+@log_plugin.json()
+async def token(method, new_token):
+    if method == POST:
+        log_plugin.auth_token = new_token
+    return ''
+
+
+@log_plugin.json()
+async def log_level(method, level):
+    if method == POST:
+        log.LOG_LEVEL = log.LABEL_TO_INT.get(level, log.INFO)
+    return log.INT_TO_LABEL.get(log.LOG_LEVEL, 'undefined for {}'.format(log.LOG_LEVEL))
+
+
+@log_plugin.plain()
+async def logs(verb, _):
+    return log.stream_web_log()
+
+
+@log_plugin.plain()
+async def log_freq(verb, _):
+    return log.stream_web_log_frequency()
+
+
+@log_plugin.json()
+async def logcfg(verb, cfg):
+    if verb == POST:
+        log.LOG_LEVEL = cfg.get('log_level', log.LOG_LEVEL)
+        log.WEB_LOG_LEVEL = cfg.get('web_log_level', log.WEB_LOG_LEVEL)
+        log.WEB_LOG_SIZE = cfg.get('web_log_size', log.WEB_LOG_SIZE)
+    return dict(log_level=log.LOG_LEVEL,
+                web_log_level=log.WEB_LOG_LEVEL,
+                web_log_size=log.WEB_LOG_SIZE)
